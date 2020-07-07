1ea2df01fa6e8ebe2e9b7a034538cd39ebac9355
Veronika Belova <belovaveronika14@gmail.com>
2020-07-07 19:19:24 +0300

    Create TransactionInputModel

---
+++ TransactionStore.API/TransactionStore.API/Models/Input/TransactionInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionStore.API.Models.Input
+{
+    public class TransactionInputModel
+    {
+        public byte TypeId { get; set; }
+        public byte CurrencyId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
