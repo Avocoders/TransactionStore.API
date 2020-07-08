d58f21dcc49a636f5a616e183c1678e4e21d97e2
Veronika Belova <belovaveronika14@gmail.com>
2020-07-08 12:53:21 +0300

    Add Dtos

---
+++ TransactionStore.API/TransactionStore.Data/DTO/CurrencyDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransactionStore.Data.DTO
+{
+    public class CurrencyDto
+    {
+        public byte? Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
