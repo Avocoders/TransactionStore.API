27770469492f701fc329dd87e8602d78b4e24fe5
Veronika Belova <belovaveronika14@gmail.com>
2020-07-08 15:15:48 +0300

    For Transaction Type

---
+++ TransactionStore.API/TransactionStore.API/Shared/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionStore.API.Shared
+{
+    public class Enums
+    {
+        public enum TransactionType
+        {
+            Deposit=1,
+            WithDrow,
+            Transfer
+        }
+    }
+}
