8d1fa0b2dd2cb5eb68a4f2be721a4c790576a80d
Veronika Belova <belovaveronika14@gmail.com>
2020-07-14 19:42:14 +0300

    Create SearchParametersInputModel

---
+++ TransactionStore.API/TransactionStore.API/Models/Input/SearchParametersInputModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionStore.API.Models.Input
+{
+    public class SearchParametersInputModel
+    {
+        public long LeadId { get; set; }
+        public string Type { get; set; }
+        public string Currency { get; set; }
+        public decimal Amount { get; set; }
+        public string FromDate { get; set; }
+        public string TillDate { get; set; }
+    }
+}
