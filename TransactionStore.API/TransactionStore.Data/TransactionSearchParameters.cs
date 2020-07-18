8a61b4b4442d8a0219de4a38d50d6b27e8216e41
Veronika Belova <belovaveronika14@gmail.com>
2020-07-18 19:08:10 +0300

    Rewrite Type and Currensy in byte

--- TransactionStore.API/TransactionStore.Data/TransactionSearchParameters.cs
+++ TransactionStore.API/TransactionStore.Data/TransactionSearchParameters.cs
@@ -5,8 +5,8 @@ namespace TransactionStore.Data
     public class TransactionSearchParameters
     {
         public long? LeadId { get; set; }
-        public string Type { get; set; }
-        public string Currency { get; set; }
+        public byte Type { get; set; }
+        public byte Currency { get; set; }
         public decimal? Amount { get; set; }
         public DateTime? FromDate { get; set; } = null;
         public DateTime? TillDate { get; set; } = null;
